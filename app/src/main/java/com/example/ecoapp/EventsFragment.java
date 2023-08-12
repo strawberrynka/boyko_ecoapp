609735a46ffe2a6f941875d02a0da67120a55292
Sofiya Boyko <sonya.bojkovk@mail.ru>
2023-08-13 01:16:19 +0300

    Events Page has been completed.

--- app/src/main/java/com/example/ecoapp/EventsFragment.java
+++ app/src/main/java/com/example/ecoapp/EventsFragment.java
@@ -3,40 +3,33 @@ package com.example.ecoapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EventsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.ecoapp.Adapter.ComingAdapter;
+import com.example.ecoapp.Model.Coming;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class EventsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
+    private RecyclerView comingRecyclerView;
+
     public EventsFragment() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EventsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static EventsFragment newInstance(String param1, String param2) {
         EventsFragment fragment = new EventsFragment();
         Bundle args = new Bundle();
@@ -58,7 +51,21 @@ public class EventsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+
+        comingRecyclerView = view.findViewById(R.id.comingRecyclerView);
+        comingRecyclerView.setHasFixedSize(true);
+        comingRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+
+        List<Coming> comingList = new ArrayList<>();
+        comingList.add(new Coming(R.drawable.coming, "Cубботник в парке Горького"));
+        comingList.add(new Coming(R.drawable.coming, "Cубботник в парке Горького"));
+        comingList.add(new Coming(R.drawable.coming, "Cубботник в парке Горького"));
+        comingList.add(new Coming(R.drawable.coming, "Cубботник в парке Горького"));
+
+        ComingAdapter comingAdapter = new ComingAdapter(comingList);
+        comingRecyclerView.setAdapter(comingAdapter);
+
+        return view;
     }
 }
--- app/src/main/java/com/example/ecoapp/MainActivity.java
+++ app/src/main/java/com/example/ecoapp/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new HomeFragment());
+        replaceFragment(new EventsFragment());
 
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
 
